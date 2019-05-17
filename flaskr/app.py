ef1bab56d4447fb1d9faf4d606f86367b74a103d
aziz belkhir <aziz.belkhir.aziz@gmail.com>
2019-05-17 13:58:04 +0200

    First commit!

--- flaskr/app.py
+++ flaskr/app.py
@@ -29,7 +29,7 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
+    @app.route('/')
     def hello():
         return 'Hello, World!'
     from . import db
