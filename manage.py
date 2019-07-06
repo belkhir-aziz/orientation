79b1a9d6ea7e749e99673005275166371a16bca5
aziz belkhir <aziz.belkhir.aziz@gmail.com>
2019-07-06 01:34:24 +0100

    First commit for heroku

--- manage.py
+++ manage.py
@@ -7,3 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
