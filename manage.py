bb0ee33aabdc90aa2c1b229d172de738b76ee7eb
aziz belkhir <aziz.belkhir.aziz@gmail.com>
2019-07-07 09:29:53 +0100

    First commit for heroku

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
